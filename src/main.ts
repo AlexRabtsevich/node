5a68d30ce4d143bbee53e666a5ec06c4c9de0ae4
a.rabtsevich <a.rabtsevich@itransition.com>
2023-01-30 16:15:58 +0300

    add eslint,prettier,husky

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,14 @@
-import http  from 'node:http';
+import http from 'node:http';
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const getNODE = ()=> process.env.NODE
+const getNODE = () => process.env.NODE;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello world!!!!\n' + getNODE());
+  res.end(`Hello world!!!!\n${getNODE()}`);
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+server.listen(port, hostname, () => {});
